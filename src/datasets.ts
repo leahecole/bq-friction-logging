a0f1993fd03d437f3e937ec93d705174d97df654
Leah Cole <coleleah@google.com>
2025-09-03 19:54:53 +0000

    add datasets script to migrate

--- src/datasets.ts
+++ src/datasets.ts
@@ -14,10 +14,8 @@
 
 'use strict';
 
-// TODO - update import
-import {BigQuery} from '@google-cloud/bigquery';
-// TODO - update type(s)
-import type {Dataset} from '@google-cloud/bigquery';
+import {BigQueryClient} from '@google-cloud/bigquery';
+import type * as BigQueryType from '@google-cloud/bigquery';
 import {randomUUID} from 'crypto';
 
 const projectId = process.env.GCLOUD_PROJECT
@@ -26,59 +24,90 @@ const datasetId = `${RESOURCE_PREFIX}_datasets_${randomUUID()}`.replace(
   /-/gi,
   '_',
 );
-// TODO - update client initialization
-const bigquery = new BigQuery();
+
+const bigquery = new BigQueryClient();
 
 async function createDataset() {
 
-    // TODO - update what gets passed to the request
     // Specify the geographic location where the dataset should reside
-    const options = {
+    const datasetObject: BigQueryType.protos.google.cloud.bigquery.v2.IDataset = {
+      datasetReference: {
+        datasetId: datasetId,
+      },
       location: 'US',
     };
+
+    // Construct the request object.
+    const request: BigQueryType.protos.google.cloud.bigquery.v2.IInsertDatasetRequest = {
+      projectId: projectId,
+      dataset: datasetObject,
+    };
     // Create a new dataset
-    // TODO - update call and its parameters
-    const [dataset] = await bigquery.createDataset(datasetId, options);
+    const [dataset] = await bigquery.insertDataset(request) as [
+      BigQueryType.protos.google.cloud.bigquery.v2.IDataset,
+      BigQueryType.protos.google.cloud.bigquery.v2.IInsertDatasetRequest | undefined,
+      {} | undefined,
+    ];
     console.log(`Dataset ${dataset.id} created.`);
   }
 async function listDatasets(){
-    // Lists all datasets
-    // TODO - update what gets passed to the request
-    // TODO - update call and its parameters
-    const [datasets] = await bigquery.getDatasets({projectId});
+     const request: BigQueryType.protos.google.cloud.bigquery.v2.IListDatasetsRequest = {
+      projectId: projectId,
+    };
+    // List all datasets in the project
+    const [datasets] = await bigquery.listDatasets(request) as [
+      BigQueryType.protos.google.cloud.bigquery.v2.IListFormatDataset[],
+      BigQueryType.protos.google.cloud.bigquery.v2.IListDatasetsRequest | null,
+      BigQueryType.protos.google.cloud.bigquery.v2.IDatasetList,
+    ];
     console.log('Datasets:');
-    datasets.forEach((dataset: Dataset) => console.log(dataset.id));
+    datasets.forEach((dataset: BigQueryType.protos.google.cloud.bigquery.v2.IDataset) => console.log(dataset.id));
 }
 
-// Get info about the dataset, update its description
+// // Get info about the dataset, update its description
 async function updateDataset(datasetId: string){
-    // TODO - update what gets passed to the request
-    // TODO - update call(s) and its parameters
-    const dataset = bigquery.dataset(datasetId)
-    const [datasetInfo] = await dataset.getMetadata();
+    const getRequest: BigQueryType.protos.google.cloud.bigquery.v2.IGetDatasetRequest = {
+      projectId: projectId,
+      datasetId: datasetId,
+    };
+    const dataset = await bigquery.getDataset(getRequest)
     console.log("Dataset info:")
-    console.log(datasetInfo)
+    console.log(dataset)
     // Set new dataset description
     const description = 'New dataset description.';
-    datasetInfo.description = description;
-
-    // TODO - update what gets passed to the request
-    // TODO - update call and its parameters
-    const [apiResponse] = await dataset.setMetadata(datasetInfo);
-    const newDescription = apiResponse.description;
+    const datasetToUpdate = {
+      projectId: projectId,
+      datasetId: datasetId,
+      datasetReference: {
+        datasetId: datasetId,
+      },
+      description: {value: description},
+    };
+    // Construct the request object.
+    const updateRequest: BigQueryType.protos.google.cloud.bigquery.v2.IUpdateOrPatchDatasetRequest = {
+      projectId: projectId,
+      datasetId: datasetId,
+      dataset: datasetToUpdate,
+    };
+    const [updateResponse] = await bigquery.updateDataset(updateRequest) as [
+      BigQueryType.protos.google.cloud.bigquery.v2.IDataset,
+      BigQueryType.protos.google.cloud.bigquery.v2.IUpdateOrPatchDatasetRequest | undefined,
+      {} | undefined,
+    ];
+    const newDescription = updateResponse.description!.value;
 
     console.log(`${datasetId} description: ${newDescription}`);
 
 
 }
   async function deleteDataset(datasetId: string) {
-    // TODO - update what gets passed to the request
-    // Create a reference to the existing dataset
-    const dataset = bigquery.dataset(datasetId);
-    // TODO - update call and its parameters
-    // Delete the dataset and its contents
-    await dataset.delete({force: true});
-    console.log(`Dataset ${dataset.id} deleted.`);
+    const deleteRequest: BigQueryType.protos.google.cloud.bigquery.v2.IDeleteDatasetRequest = {
+      projectId: projectId,
+      datasetId: datasetId,
+      deleteContents: true, // Set to true to delete all tables in the dataset
+    };
+    await bigquery.deleteDataset(deleteRequest);
+      console.log(`Dataset ${datasetId} deleted.`);
   }
 // wrap in an async main function so we can make calls in order
 async function main(){
